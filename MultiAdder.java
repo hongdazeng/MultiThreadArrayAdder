48b44980e3229053523f07f1281495b4b1651535
Hongda Zeng <hongda.code@gmail.com>
2014-11-04 20:42:11 -0500

    create a array to test

---
+++ MultiAdder.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class MultiAdder {
+    public static void main(String[] args) {
+        int[] thelist = new int[100];
+        for (int i = 0; i < thelist.length; i++) {
+            thelist[i] = i;
+        }
+    }
+}
